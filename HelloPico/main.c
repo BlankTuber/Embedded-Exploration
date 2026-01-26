94898ed0390bd95293059ae08659d935ef3f27f9
BlankTuber <elias.neerbye@gmail.com>
2026-01-26 14:02:18 +0100

    Fixed my formatter

--- HelloPico/main.c
+++ HelloPico/main.c
@@ -16,36 +16,36 @@
 // Initialize
 void pico_led_init(void) {
 #ifdef PICO_DEFAULT_LED_PIN
-  gpio_init(PICO_DEFAULT_LED_PIN);
-  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 #endif
 }
 
 // Increase delay each time
 void increase_delay(int *delay) {
-  if (*delay >= 1000) {
-    *delay = 0;
-  }
-  *delay += 50;
+    if (*delay >= 1000) {
+        *delay = 0;
+    }
+    *delay += 50;
 }
 
 // Turn the LED on and off based on boolean value
 void pico_set_led(bool led_on, int *delay) {
 #if defined(PICO_DEFAULT_LED_PIN)
-  gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-  if (delay != NULL) {
-    increase_delay(delay);
-  }
+    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+    if (delay != NULL) {
+        increase_delay(delay);
+    }
 #endif
 }
 
 int main() {
-  pico_led_init();
-  int delay = LED_DELAY_MS;
-  while (true) {
-    pico_set_led(true, &delay);
-    sleep_ms(delay);
-    pico_set_led(false, NULL);
-    sleep_ms(delay);
-  }
+    pico_led_init();
+    int delay = LED_DELAY_MS;
+    while (true) {
+        pico_set_led(true, &delay);
+        sleep_ms(delay);
+        pico_set_led(false, NULL);
+        sleep_ms(delay);
+    }
 }
