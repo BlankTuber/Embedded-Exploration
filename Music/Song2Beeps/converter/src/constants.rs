8879bdce174d3ae9917b3e770c6a007b0bdd515a
BlankTuber <elias.neerbye@gmail.com>
2026-02-04 15:49:47 +0100

    Since this worked much worse than i though, ill let you all enjoy some great super mario bros music

--- Music/Song2Beeps/converter/src/constants.rs
+++ Music/Song2Beeps/converter/src/constants.rs
@@ -1,6 +1,6 @@
 pub const NOTE_NAMES: [&str; 12] = [
     "C", "CS", "D", "DS", "E", "F", "FS", "G", "GS", "A", "AS", "B",
 ];
-pub const TARGET_TONE_MS: i8 = 10;
-pub const VOICES: i8 = 3;
-pub const TOTAL_CHUNK_MS: i8 = TARGET_TONE_MS * VOICES;
+pub const TARGET_TONE_MS: i16 = 10;
+pub const VOICES: i16 = 10;
+pub const TOTAL_CHUNK_MS: i16 = TARGET_TONE_MS * VOICES;
