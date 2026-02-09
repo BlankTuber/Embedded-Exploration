1b3e21b8c3408c824d35cf0b3fe965201a15a76e
BlankTuber <elias.neerbye@gmail.com>
2026-02-09 15:14:16 +0100

    Fixed some build issues for initial setup

--- HelloLED/main.c
+++ HelloLED/main.c
@@ -1,3 +1,9 @@
+// This code is based off the example from the "pico-examples" repository:
+// https://github.com/raspberrypi/pico-examples/blob/master/pio/ws2812/ws2812.c
+
+#include "hardware/clocks.h"
+#include "hardware/pio.h"
 #include "pico/stdlib.h"
+#include "ws2812.pio.h"
 
 int main() { return 0; }
